f9c645ce5fdfa4d58b3af5a4ab422362fc978ae8
priyanshuborole <ppborole1762002@gmail.com>
2023-06-15 18:53:59 +0530

    :white_check_mark: implemented type converters for roomdb

--- app/src/main/java/com/priyanshub/boruteapp/data/local/BorutoDatabase.kt
+++ app/src/main/java/com/priyanshub/boruteapp/data/local/BorutoDatabase.kt
@@ -2,12 +2,14 @@ package com.priyanshub.boruteapp.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.priyanshub.boruteapp.data.local.dao.HeroDao
 import com.priyanshub.boruteapp.data.local.dao.HeroRemoteKeyDao
 import com.priyanshub.boruteapp.domain.model.Hero
 import com.priyanshub.boruteapp.domain.model.HeroRemoteKey
 
 @Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
+@TypeConverters(DatabaseConverter::class)
 abstract class BorutoDatabase: RoomDatabase() {
     abstract fun heroDao(): HeroDao
 
